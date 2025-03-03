52fddf0be8c81a311b56cc6db09bf24aa68426e3
unknown <dan.tecu@ymail.com>
2025-03-03 14:31:37 +0100

    Adding balanced oversampled trainer

---
+++ lib/balancedOversampledTrainer.py
@@ -0,0 +1,18 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class BalancedOversampledTrainer(Trainer):
+    def __init__(self, y, **kwargs):
+        super().__init__(kwargs)
+        self.y = y
+
+    def createBalancedOversampledDataLoader(self):
+        categoryCounts = torch.bincount(self.y)
+        weights = 1. / categoryCounts.float()
+        sampleWeights = weights[self.y]
+        sampler = torch.utils.data.WeightedRandomSampler(sampleWeights, len(sampleWeights))
+        dataset = torch.utils.data.TensorDataset(self.x, self.y)
+        return DataLoader(self.train_dataset, batch_size=8, sampler=sampler)
+
+    def get_train_dataloader(self):
+        return custom_dataloader
